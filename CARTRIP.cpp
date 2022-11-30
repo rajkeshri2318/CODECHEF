0d12ccb63d57771058851cb03d8c8eb74db57e40
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2022-11-30 19:52:14 +0530

    Create CARTRIP.cpp

---
+++ CARTRIP.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while (t--) {
+	    int x;
+	    cin>>x;
+	    
+	    if (x>300) {
+	        cout<<x*10<<endl;
+	    }
+	    else {
+	        cout<<300*10<<endl;
+	    }
+	}
+	return 0;
+}
