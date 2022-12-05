bdb29ff84cb8c9ef2df2fa2c9a618379e64ca52f
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2022-12-06 00:16:29 +0530

    Create APPLEORANGE.cpp

---
+++ APPLEORANGE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,a,b;
+	cin>>x;
+	cin>>a>>b;
+	
+	if (a+b <= x) {
+	    cout<<"YES"<<endl;
+	}
+	else {
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
