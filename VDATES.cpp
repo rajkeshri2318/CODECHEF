a52aecc2f85694fbea10f078f613c372b971efef
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2022-12-10 19:09:46 +0530

    Create VDATES.cpp

---
+++ VDATES.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while (t--) {
+        int D,L,R;
+        cin>>D>>L>>R;
+        
+        if (D>=L && D<=R) {
+            cout<<"Take second dose now"<<endl;
+        }
+        else if (D>=L && D>=R) {
+            cout<<"Too Late"<<endl;
+        }
+        else {
+            cout<<"Too Early"<<endl;
+        }
+    }
+	return 0;
+}
