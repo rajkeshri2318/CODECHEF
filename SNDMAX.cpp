ec76d42f400f1d29d3607de669cb5705bd590a6f
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2022-12-17 16:59:48 +0530

    Create SNDMAX.cpp

---
+++ SNDMAX.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	int a[3];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[0]>>a[1]>>a[2];
+		sort(a,a+3);
+		cout<<a[1]<<endl;
+	}
+	return 0;
+}
