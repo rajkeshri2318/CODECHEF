f0464de0a1fa8b6e51c7960bfb6440da359ca18f
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2022-12-19 23:22:59 +0530

    Create DETSCORE.cpp

---
+++ DETSCORE.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb emplace_back
+//#define ll long long
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int x,y;cin>>x>>y;
+        cout<<(x/10)*y<<endl;
+    }
+    return 0;
+}
