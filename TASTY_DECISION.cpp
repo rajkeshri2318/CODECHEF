f382b58460341efe85f20b38c36458ccc81a1eef
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2022-12-23 23:46:13 +0530

    Create TASTY_DECISION.cpp

---
+++ TASTY_DECISION.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        if(2*x>5*y)
+        cout<<"chocolate"<<endl;
+        else if(2*x==5*y)
+        cout<<"either"<<endl;
+        else
+        cout<<"candy"<<endl;
+    }
+    
+    
+	return 0;
+}
