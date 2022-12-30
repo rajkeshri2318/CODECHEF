9012e62d42b9165eabd99032b36f670a07eac67a
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2022-12-30 21:00:01 +0530

    Create PASSES.cpp

---
+++ PASSES.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while (t--) {
+        int n,k;
+        cin>>n>>k;
+        
+        if (k>=n+1) {
+            cout<<"YES"<<endl;
+        }
+        else {
+            cout<<"NO"<<endl;
+        }
+    }
+	return 0;
+}
