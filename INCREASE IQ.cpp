e0cfd13e857dbf63efb178ac9677283f60ddef00
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-04-05 16:27:10 +0530

    Create INCREASE IQ.cpp

---
+++ INCREASE IQ.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X,Y;
+	cin>>X>>Y;
+	
+	if((X+7)> 170) {
+	    cout<<"YES"<<endl;
+	}
+	else {
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
