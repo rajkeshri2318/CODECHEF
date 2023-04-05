12cb020f222c0f2a6e2870650dcc1ea93f727277
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-04-05 16:12:01 +0530

    Create AIRLINES.cpp

---
+++ AIRLINES.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int X,Y,Z;
+	    cin>>X>>Y>>Z;
+	    
+	    if (Y<=(X*10)) {
+	        cout<<Y*Z<<endl;
+	    }
+	    else {
+	        cout<<X*10*Z<<endl;
+	    }
+	}
+	return 0;
+}
