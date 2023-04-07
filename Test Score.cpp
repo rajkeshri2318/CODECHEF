e2f251da395d7d8692fc63872f5abdb0964b1435
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-04-07 21:44:46 +0530

    Create Test Score.cpp

---
+++ Test Score.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int t;
+    cin>>t;
+    
+    while(t--) {
+        int N,X,Y;
+        cin>>N>>X>>Y;
+        
+        if(Y%X==0 && Y%X<=N) {
+            cout<<"YES"<<endl;
+        }
+        else {
+            cout<<"NO"<<endl;
+        }
+    }
+}
