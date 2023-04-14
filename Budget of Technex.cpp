a7f9deb67b7c26bb0457344f7310f5f20a0db36d
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-04-14 22:01:11 +0530

    Create Budget of Technex.cpp

---
+++ Budget of Technex.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int R;
+	    cin>>R;
+	    int Z =(R*1000)/2;
+	    cout<<Z/5<<endl;
+	}
+	return 0;
+}
