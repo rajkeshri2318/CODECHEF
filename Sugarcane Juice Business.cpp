2656e02a8739632eaf05628e81b6a1ad77af7529
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-04-19 23:05:59 +0530

    Create Sugarcane Juice Business.cpp

---
+++ Sugarcane Juice Business.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int x=50-(50*0.70);
+	    cout<<x*n<<endl;
+	}
+	return 0;
+}
