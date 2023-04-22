1f78dba07a9904dadaab0f0f4aaa357d902f5f41
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-04-22 11:14:06 +0530

    Create Candy Division.cpp

---
+++ Candy Division.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int n;
+	    cin>>n;
+	    
+	    if(n%3 == 0) {
+	        cout<<"yes"<<endl;
+	    }
+	    else {
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
