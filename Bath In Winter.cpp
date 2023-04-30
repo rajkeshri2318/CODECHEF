d0652b6a868fb81b2a01ef064e1fdc8576a68965
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-04-30 23:09:58 +0530

    Create Bath In Winter.cpp

---
+++ Bath In Winter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int X,Y;
+	    cin>>X>>Y;
+	    
+	    
+	    
+	    cout<<X / (2*Y)<<endl;
+	}
+	return 0;
+}
