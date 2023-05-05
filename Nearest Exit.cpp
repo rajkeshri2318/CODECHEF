d6425396b11cfddbbd84128ff0f2b0023479c6ca
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-05-05 23:17:00 +0530

    Create Nearest Exit.cpp

---
+++ Nearest Exit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int x;
+	    cin>>x;
+	    
+	    if (x<=50) cout<<"left"<<endl;
+	    else cout<<"right"<<endl;
+	}
+	return 0;
+}
