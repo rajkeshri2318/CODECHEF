fd8932deaf82efcf527b0ac93ec06208bfa64281
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-05-12 13:45:17 +0530

    Create Chef and Wire Frames.cpp

---
+++ Chef and Wire Frames.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m,x;
+	    cin>>n>>m>>x;
+	    cout<<((2*(n+m))*x)<<endl;
+	}
+	return 0;
+}
